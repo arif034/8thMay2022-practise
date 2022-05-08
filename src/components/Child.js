fba33acd497b377e187866b972186a625c62e6b8
arif034 <45455917+arif034@users.noreply.github.com>
2022-05-08 13:31:39 +0530

    both parent to child props transfer and child to parent props transfer in one code both parent to child props transfer and child to parent props transfer in one code

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,5 +1,19 @@
 import { Component } from "react";
 
-class Child extends Component {}
-
+class Child extends Component {
+  render(props) {
+    return (
+      <div>
+        <br />
+        <button
+          onClick={() => {
+            this.props.updateName(this.props.name);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
 export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,8 +1,43 @@
 import { Component } from "react";
-
+import Child from "./Child.js";
 class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      tempName: ""
+    };
+  }
+
+  updateName = (name) => {
+    this.setState(() => {
+      return { name: name };
+    });
+  };
+
+  updateTempName = (event) => {
+    this.setState(() => {
+      return {
+        tempName: event.target.value
+      };
+    });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter name here..."
+          onChange={this.updateTempName}
+          value={this.state.tempName}
+        />
+        <Child name={this.state.tempName} updateName={this.updateName} />
+        <br />
+        My name is {this.state.name}
+      </div>
+    );
   }
 }
 export default Parent;
