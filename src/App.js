94a2c413bc136d26f52e030ab3331aeb3e0fdd3b
arif034 <45455917+arif034@users.noreply.github.com>
2022-05-08 11:34:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import Parent from "./components/Parent.js";
+export default function App() {
+  return (
+    <div className="App">
+      <Parent />
+    </div>
+  );
+}
---
+++ src/components/Parent.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  incrementCount = () => {
+    this.setState((prevState) => {
+      return prevState.count < 10
+        ? { count: prevState.count + 1 }
+        : { count: 10 };
+    });
+  };
+
+  decrementCount = () => {
+    this.setState((prevState) => {
+      return prevState.count > 0
+        ? { count: prevState.count - 1 }
+        : { count: 0 };
+    });
+  };
+
+  resetCount = () => {
+    this.setState((prevState) => {
+      return { count: 0 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.incrementCount}> Increment</button>
+        <br />
+        <button onClick={this.decrementCount}> Decrement</button>
+        <br />
+        <button onClick={this.resetCount}> Reset</button>
+        <br />
+        <br />
+        {this.state.count}
+      </div>
+    );
+  }
+}
+export default Parent;
